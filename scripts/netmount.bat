b45eee3a8d1e96918d046f8cfd6d9f9a387f3c9f
ayyyyyyyy <mndevsec1@gmail.com>
2016-12-29 10:10:05 -0500

    added netmount

---
+++ scripts/netmount.bat
@@ -0,0 +1,5 @@
+:: Network Share Mount Template
+NET use Q: \\x.x.com\folder1\folder2\folder_3\Actual_Directory_to_Mount /persistent:yes
+NET use R: \\x.x.com\folder2\folder2\folder_3\Actual_Directory_to_Mount /persistent:yes
+NET use S: \\x.x.com\folder3\folder2\folder_3\Actual_Directory_to_Mount /persistent:yes
+NET use T: \\x.x.com\folder4\folder2\folder_3\Actual_Directory_to_Mount /persistent:yes
